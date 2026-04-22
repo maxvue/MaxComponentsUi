0a90034dd9fac11cda999ff601a263ab1e00d35a
Johnattas Santana <johnattas@gmail.com>
2026-04-22 14:59:02 -0300

    0.1.0

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export { default as Button } from './components/MaxButton.vue';
 export { default as MaxIcon } from './components/MaxIcon.vue';
 export { default as MaxInputText } from './components/MaxInputText.vue';
 export { default as InputText } from './components/MaxInputText.vue';
+export { default as Grid } from './components/Grid.vue';
 
 export default function install(app: App): any {
     app.use(PrimeVue, {
