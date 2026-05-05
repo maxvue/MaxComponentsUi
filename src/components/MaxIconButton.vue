45811589b5c7c71e814df33d7a65aaee19c678da
Johnattas Santana <johnattas@gmail.com>
2026-05-05 13:00:01 -0300

    feat: atualizando componentes

--- src/components/MaxIconButton.vue
+++ src/components/MaxIconButton.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="icon-div ico-btn" ref="icon_ref" :icon="attrs.icon ?? attrs.i" v-bind="attrs">
-        <IconZ v-bind="attrs" v-tooltip="null" pointer @click.stop="execute" v-if="attrs.blank || attrs.route" />
-        <IconZ v-bind="attrs" v-tooltip="null" pointer v-else />
+        <MaxIcon v-bind="attrs" v-tooltip="null" pointer @click.stop="execute" v-if="attrs.blank || attrs.route" />
+        <MaxIcon v-bind="attrs" v-tooltip="null" pointer v-else />
         <div class="sub-icon checked" v-if="attrs.checked === true">
             <div class="background-icon"></div>
             <svg full xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.6 13.8l-2.15-2.15q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7L9.9 15.9q.3.3.7.3t.7-.3l5.65-5.65q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" /></svg>
