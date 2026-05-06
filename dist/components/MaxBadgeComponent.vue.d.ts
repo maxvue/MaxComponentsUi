ea42f4d830bd6e9dc400dc35ba073bcf61dda170
Johnattas Santana <johnattas@gmail.com>
2026-05-06 19:17:37 -0300

    feat: atualizando componentes

--- dist/components/MaxBadgeComponent.vue.d.ts
+++ dist/components/MaxBadgeComponent.vue.d.ts
@@ -1,12 +1,47 @@
-declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {
+type __VLS_Props = {
+    /** Nome do ícone (ex: 'mdi:home') */
+    icon?: string;
+    /** Alias para o nome do ícone */
+    i?: string;
+    /** Texto do badge */
+    label?: string;
+    /** Alias para o texto do badge */
+    value?: string;
+    /** Alias para o texto do badge */
+    msg?: string;
+    /** Alias para o texto do badge */
+    mensagem?: string;
+    /** Alias para o texto do badge */
+    text?: string;
+    /** Alias para o texto do badge */
+    txt?: string;
+    /** Alias para o nome do ícone */
+    number?: string;
+    /** Rotação do ícone em graus */
+    rotate?: number;
+    /** Inversão do ícone */
+    flip?: 'horizontal' | 'vertical' | 'h' | 'v' | 'x' | 'y' | 'xy';
+    /** Tamanho do ícone (em px ou multiplicador) */
+    size?: string | number;
+    /** Alias para o tamanho */
+    scale?: string | number;
+    /** Largura específica */
+    width?: string | number;
+    /** Altura específica */
+    height?: string | number;
+    /** Cor do ícone */
+    iconColor?: string;
+    /** Valor do ícone */
+    iconValue?: string;
+    /** Apenas se estiver usando overlay = true */
+    badge?: any;
+    /** Apenas se estiver usando overlay = true */
+    overlay?: boolean | undefined;
+};
+declare const _default: import('vue').DefineComponent<__VLS_Props, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {
     badgeElem: {
         $props: import('primevue/badge').BadgeProps & import('vue').VNodeProps & import('vue').AllowedComponentProps & import('vue').ComponentCustomProps;
         $slots: import('primevue/badge').BadgeSlots;
-        $emit: (e: string, ...args: any[]) => void;
-    } | null;
-    badge: {
-        $props: import('primevue/overlaybadge').OverlayBadgeProps & import('vue').VNodeProps & import('vue').AllowedComponentProps & import('vue').ComponentCustomProps;
-        $slots: import('primevue/overlaybadge').OverlayBadgeSlots;
         $emit: (e: string, ...args: any[]) => void;
     } | null;
 }, HTMLDivElement>;
--- src/components/MaxBadgeComponent.vue
+++ src/components/MaxBadgeComponent.vue
@@ -1,31 +1,62 @@
 <template>
-    <div :class="`badge-component-main-div ${attrs.size ?? ''}`">
-        <Icon v-if="attrs.icon" :i="attrs.icon" class="icon-badge" v-bind="attrs" size="1.1" />
-        <Badge v-bind="attrs" :value="(attrs.label ?? attrs.msg ?? attrs.value ?? attrs.mensagem ?? attrs.text ?? attrs.txt ?? attrs.number) as any" v-if="attrs.overlay === undefined" :class="`${attrs.icon || attrs['icon-color'] || attrs['iconcolor'] || attrs['iconColor'] ? 'with-icon' : ''} ${attrs.iconValue || attrs['icon-value'] || attrs['icon-value'] ? 'with-icon-value' : ''}`" ref="badgeElem" />
-        <OverlayBadge v-bind="attrs" v-else ref="badge" />
+    <div :class="`badge-component-main-div ${props.size ?? ''}`">
+        <MaxIcon v-if="props.icon || props.i" :icon="props.icon ?? props.i" class="icon-badge" size="1.1" />
+        <Badge :value="(props.label ?? props.msg ?? props.value ?? props.mensagem ?? props.text ?? props.txt ?? props.number) as any" v-if="props.overlay === undefined" :class="`${props.icon || props.iconColor ? 'with-icon' : ''} ${props.iconValue ? 'with-icon-value' : ''}`" ref="badgeElem" />
+        <OverlayBadge v-if="props.overlay" />
         <div class="circle-color-badge">
-            <div :style="{ background: (attrs['icon-color'] ?? attrs['iconcolor'] ?? attrs['iconColor'] ?? 'none') as string }" class="circle-color-badge-text">
-                {{ attrs['icon-value'] ?? '' }}
+            <div :style="{ background: (props.iconColor ?? 'none') as string }" class="circle-color-badge-text">
+                {{ props.iconValue ?? '' }}
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    import { ref, onMounted, useAttrs } from 'vue';
     import Badge from 'primevue/badge';
     import OverlayBadge from 'primevue/overlaybadge';
-    const attrs = useAttrs();
-    const badgeElem = ref(null);
+    import MaxIcon from './MaxIcon.vue';
 
-    onMounted(() => {
-        if (badgeElem.value) {
-            if (attrs['background'] || attrs['background-color'] || attrs['backgroundColor']) (badgeElem.value as any).$el.style.backgroundColor = attrs['background'] ?? attrs['background-color'] ?? attrs['backgroundColor'];
+    const props = defineProps<{
+        /** Nome do ícone (ex: 'mdi:home') */
+        icon?: string;
+        /** Alias para o nome do ícone */
+        i?: string;
+        /** Texto do badge */
+        label?: string;
+        /** Alias para o texto do badge */
+        value?: string;
+        /** Alias para o texto do badge */
+        msg?: string;
+        /** Alias para o texto do badge */
+        mensagem?: string;
+        /** Alias para o texto do badge */
+        text?: string;
+        /** Alias para o texto do badge */
+        txt?: string;
+        /** Alias para o nome do ícone */
+        number?: string;
+        /** Rotação do ícone em graus */
+        rotate?: number;
+        /** Inversão do ícone */
+        flip?: 'horizontal' | 'vertical' | 'h' | 'v' | 'x' | 'y' | 'xy';
+        /** Tamanho do ícone (em px ou multiplicador) */
+        size?: string | number;
+        /** Alias para o tamanho */
+        scale?: string | number;
+        /** Largura específica */
+        width?: string | number;
+        /** Altura específica */
+        height?: string | number;
+        /** Cor do ícone */
+        iconColor?: string;
+        /** Valor do ícone */
+        iconValue?: string;
+        /** Apenas se estiver usando overlay = true */
+        badge?: any;
+        /** Apenas se estiver usando overlay = true */
+        overlay?: boolean | undefined;
 
-            if (attrs['font-color'] || attrs['fontcolor'] || attrs['fontColor']) (badgeElem.value as any).$el.style.color = attrs['font-color'] ?? attrs['fontcolor'] ?? attrs['fontColor'];
-
-        }
-    });
+    }>();
 </script>
 
 <style lang="scss">
