8ac067a7788222059e0186d5c3ae80bff112cfb5
Johnattas Santana <johnattas@gmail.com>
2026-05-06 08:05:41 -0300

    feat: atualizando componentes

---
+++ src/components/MaxGridCols.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="grid-cols" v-bind="attrs">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const attrs: any = useAttrs();
+</script>
+
+<style lang="scss" scoped>
+    .grid-cols {
+        display: grid;
+        place-items: center;
+        height: auto;
+        position: relative;
+        grid-template-columns: repeat(24, 1fr);
+        width: 100%;
+        gap: 15px 10px;
+    }
+</style>
