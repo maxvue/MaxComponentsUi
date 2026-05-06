52e543135d6122b84be64d23f2b5e49a2851b226
Johnattas Santana <johnattas@gmail.com>
2026-05-06 07:45:53 -0300

    feat: atualizando componentes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import ptBR from './locales/pt-br';
 
 export * from './components/_primeVue';
 export * from './helpers/resolver';
+export * from './unoCssPreset';
 
 export { default as MaxIcon } from './components/MaxIcon.vue';
 export { default as MaxGrid } from './components/MaxGrid.vue';
