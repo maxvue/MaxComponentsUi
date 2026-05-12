14705de670ee54d69f4fb848e9c6dafbe2aa85cb
Johnattas Santana <johnattas@gmail.com>
2026-05-12 08:56:11 -0300

    feat: atualizando componentes

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
         },
         rollupOptions: {
             external: [
+                'vue-router',
                 'vue',
                 '@iconify/vue',
                 'sass',
