e3827fc9990fadd46035e11ff3b8a2690ba64f9e
Johnattas Santana <johnattas@gmail.com>
2026-05-13 11:34:01 -0300

    feat: atualizando componentes

--- src/presetMaxUno.ts
+++ src/presetMaxUno.ts
@@ -32,13 +32,13 @@ export const presetMaxUno = () => {
                 // Tipografia
                 [/^font-weight-(.+)$/, ([, s]) => ({ 'font-weight': s })],
                 [/^w-?max-(.+)$/, ([, s]) => ({ 'max-width': s + 'px' })],
-                [/^max-?w-(.+)$/, ([, s]) => ({ 'max-width': s + 'px' })],
+                [/^max-w-(.+)$/, ([, s]) => ({ 'max-width': s + 'px' })],
                 [/^h-?max-(.+)$/, ([, s]) => ({ 'max-height': s + 'px' })],
-                [/^max-?h-(.+)$/, ([, s]) => ({ 'max-height': s + 'px' })],
+                [/^max-h-(.+)$/, ([, s]) => ({ 'max-height': s + 'px' })],
                 [/^w-?min-(.+)$/, ([, s]) => ({ 'min-width': s + 'px' })],
-                [/^min-?w-(.+)$/, ([, s]) => ({ 'min-width': s + 'px' })],
+                [/^min-w-(.+)$/, ([, s]) => ({ 'min-width': s + 'px' })],
                 [/^h-?min-(.+)$/, ([, s]) => ({ 'min-height': s + 'px' })],
-                [/^min-?h-(.+)$/, ([, s]) => ({ 'min-height': s + 'px' })],
+                [/^min-h-(.+)$/, ([, s]) => ({ 'min-height': s + 'px' })],
 
                 [/^hover-(.+)$/, ([, s]) => ({
                     '&:hover': {
