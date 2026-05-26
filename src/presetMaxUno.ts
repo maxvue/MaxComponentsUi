6b45d97aa39af57916cf60e6b0fb3a7dc7605b53
Johnattas Santana <johnattas@gmail.com>
2026-05-26 17:48:07 -0300

    feat: atualizando componentes

--- src/presetMaxUno.ts
+++ src/presetMaxUno.ts
@@ -12,6 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const presetMaxUno = definePreset(() => {
+    console.log('MAX UNOOOO');
     return {
         name: 'max-css-preset',
         // RULES: CSS customizado que não existe no UnoCSS padrão
